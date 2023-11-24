ac75aded5bfe52bcd411bd5af866c7e0cd4188ef
Tushar Anekar <anekar.tushar@gmail.com>
2023-11-24 21:10:46 +0530

    feat- questions db added

---
+++ src/db/questions.js
@@ -0,0 +1,24 @@
+const questions = [
+  {
+    questionText: "What is the capital of France?",
+    answerOptions: ["New York", "London", "Paris", "Dublin"],
+    correctAnswer: "Paris",
+  },
+  {
+    questionText: "Who is CEO of Tesla?",
+    answerOptions: ["Jeff Bezos", "Elon Musk", "Bill Gates", "Tony Stark"],
+    correctAnswer: "Elon Musk",
+  },
+  {
+    questionText: "The iPhone was created by which company?",
+    answerOptions: ["Apple", "Intel", "Amazon", "Microsoft"],
+    correctAnswer: "Apple",
+  },
+  {
+    questionText: "How many Harry Potter books are there?",
+    answerOptions: ["1", "4", "6", "7"],
+    correctAnswer: "7",
+  },
+];
+
+export { questions };
